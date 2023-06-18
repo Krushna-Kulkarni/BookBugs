bb564d84d6cb4ef73565aebd1be6c61616ca3ecb
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-06-18 22:32:40 +0530

    added fafa icon

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -16,14 +16,13 @@ export const NavigationBar = () => {
                 </div>
                 <div className="searchBarDiv">
                     <input onChange={(e) => searchTextHandler(e.target.value)} type="text" className="searchbar" placeholder="Search books..." />
-                    <button onClick={searchClickHandler} className="searchbtn"><NavLink to="/products">Search</NavLink></button>
+                    <button onClick={searchClickHandler} className="searchbtn"><NavLink to="/products"><i class="fa fa-search"></i></NavLink></button>
                 </div>
                 <div className="profileDetailsDiv">
-                    <p>Logout</p>
+                    <p><i class="fa fa-sign-out" aria-hidden="true"></i></p>
                     <p><NavLink to="/cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i>({cart.length})</NavLink></p>
                     <p><NavLink to="/wishList"><i class="fa fa-heart" aria-hidden="true"></i>({wishList.length})</NavLink></p>
-
-                    <p>Profile</p>
+                    <p><i class="fa fa-user-circle-o"></i></p>
                 </div>
             </div>
         </>
