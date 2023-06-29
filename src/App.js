7bd6649c71bc06c3ac0979f3cd211a1357e19e0e
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-06-30 00:56:09 +0530

    added routes to new pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom"
 import "./App.css";
 import Mockman from "mockman-js";
-import { Home } from "./pages/Home";
+
 import { Products } from "./pages/Products/Products";
 import { Product } from "./pages/Product/Product";
 import { Cart } from "./pages/Cart/Cart";
 import { WishList } from "./pages/WishList/WishList";
 
 import { ToastContainer } from 'react-toastify';
+import { Profile } from "./pages/Profile/Profile";
+import { PageNotFound } from "./pages/PageNotFound/PageNotFound";
+import { Home } from "./pages/Home/Home";
 
 
 function App() {
@@ -21,6 +24,8 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/wishList" element={<WishList />} />
         <Route path="/mockman" element={<Mockman />} />
+        <Route path="/user" element={<Profile />} />
+        <Route path="/*" element={<PageNotFound />} />
       </Routes>
       <ToastContainer />
     </div>
