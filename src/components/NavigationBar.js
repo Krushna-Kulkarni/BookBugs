7020aa5b20b70453991a231b29a5729aff79e2d7
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-06-30 00:57:16 +0530

    fixed links and  added css in seperate file

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -4,6 +4,8 @@ import { NavLink } from "react-router-dom"
 import { CartContext } from "../contexts/CartContext"
 import { WishListContext } from "../contexts/WishListContext"
 
+import "./NavigationBar.css"
+
 export const NavigationBar = () => {
     const { searchText, searchTextHandler, searchClickHandler } = useContext(ProductsContext)
     const { cart } = useContext(CartContext);
@@ -15,7 +17,7 @@ export const NavigationBar = () => {
         <>
             <div className="navigationDiv">
                 <div className="logoDiv">
-                    <NavLink to="/products"><h1>BookBugs</h1></NavLink>
+                    <NavLink to="/"><h1>BookBugs</h1></NavLink>
                 </div>
                 <div className="searchBarDiv">
                     <input onChange={(e) => searchTextHandler(e.target.value)} value={searchText} type="text" className="searchbar" placeholder="Search books..." />
@@ -25,7 +27,7 @@ export const NavigationBar = () => {
                     <p><i className="fa fa-sign-out" aria-hidden="true"></i></p>
                     <p><NavLink to="/cart"><i className="fa fa-shopping-cart" aria-hidden="true"></i>({cart.length})</NavLink></p>
                     <p><NavLink to="/wishList"><i className="fa fa-heart" aria-hidden="true"></i>({wishList.length})</NavLink></p>
-                    <p><i className="fa fa-user-circle-o"></i></p>
+                    <p><NavLink to="/user"><i className="fa fa-user-circle-o"></i></NavLink></p>
                 </div>
             </div>
         </>
