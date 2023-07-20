dc957074e41cab1f20b0b3763d542a7c0713a185
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-07-21 00:33:41 +0530

    addded users

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -15,5 +15,61 @@ export const users = [
     password: "adarshbalika",
     createdAt: formatDate(),
     updatedAt: formatDate(),
+    addresses:[{
+      id: uuid(),
+      firstName: "Krushna",
+      lastName: "Kulkarni",
+      street: "Ganesh Nagar, Paud Road",
+      district: "Pune",
+      state: "Maharashtra",
+      pinCode: "411057",
+      phone: "1256394870"
+  }, {
+      id: uuid(),
+      firstName: "Adarsh",
+      lastName: "Balika",
+      street: "Tirupati Colony, Pangri Road",
+      district: "Beed",
+      state: "Maharashtra",
+      pinCode: "431122",
+      phone: "9420101718"
+  }]
+  },
+  {
+    _id: uuid(),
+    firstName: "Tanmay",
+    lastName: "Bhatt",
+    email: "tanmaybhatt@gmail.com",
+    password: "tanmay123",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+    addresses:[{
+      id: uuid(),
+      firstName: "Tanmay",
+      lastName: "Bhatt",
+      street: "Khar station Road",
+      district: "Mumbai",
+      state: "Maharashtra",
+      pinCode: "4110004",
+      phone: "1256394870"
+  }, {
+      id: uuid(),
+      firstName: "Samay",
+      lastName: "Raina",
+      street: "Karve Nagar, Paud Road",
+      district: "Pune",
+      state: "Maharashtra",
+      pinCode: "411057",
+      phone: "1256394870"
+  }, {
+      id: uuid(),
+      firstName: "Rohan",
+      lastName: "Joshi",
+      street: "Colaba Causeway, Colaba",
+      district: "Mumbai",
+      state: "Maharashtra",
+      pinCode: "4110002",
+      phone: "78946123074"
+  }]
   },
 ];
