ae6846ae5b4497140bfc861e4875d9c4bf480e62
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-07-24 14:56:41 +0530

    style: changed alert types

--- src/contexts/ToastContext.js
+++ src/contexts/ToastContext.js
@@ -9,19 +9,19 @@ export const ToastProvider = ({ children }) => {
     const notify = (action) => {
         switch (action) {
             case "addToCart":
-                return toast.info("Item Added to cart!", { position: "bottom-right", autoClose: 2000 })
+                return toast.success("Item Added to cart!", { position: "bottom-right", autoClose: 2000 })
             case "removeFromCart":
-                return toast.info("Item Removed From cart!", { position: "bottom-right", autoClose: 2000 })
+                return toast.error("Item Removed From cart!", { position: "bottom-right", autoClose: 2000 })
             case "addToWishlist":
-                return toast.info("Item Added To Wishlist!", { position: "bottom-right", autoClose: 2000 })
+                return toast.success("Item Added To Wishlist!", { position: "bottom-right", autoClose: 2000 })
             case "removeFromWishlist":
-                return toast.info("Item Removed From Wishlist!", { position: "bottom-right", autoClose: 2000 })
+                return toast.error("Item Removed From Wishlist!", { position: "bottom-right", autoClose: 2000 })
             case "emailPasswordIncorrect":
                 return toast.error("Email or Password is incorrect!", { position: "bottom-right", autoClose: 2000 })
             case "userLoggedIn":
                 return toast.success("Logged In Successfully!", { position: "bottom-right", autoClose: 2000 })
             case "userLoggedOut":
-                return toast.info("Logged Out Successfully!", { position: "bottom-right", autoClose: 2000 })
+                return toast.error("Logged Out Successfully!", { position: "bottom-right", autoClose: 2000 })
             case "pleaseLogin":
                 return toast.info("Please Login To Continue!", { position: "bottom-right", autoClose: 2000 })
             default:
